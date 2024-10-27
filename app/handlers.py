7f5ca43fff08dc290562c3ecd6c85a745e686408
hostyz <andreydiz1234@gmai.com>
2024-10-27 08:40:16 +0000

    new file:   app/handlers.py 	new file:   main.py

---
+++ app/handlers.py
@@ -0,0 +1,17 @@
+from aiogram.filters import Command, CommandStart
+from aiogram.types import Message
+from aiogram import F, Router
+
+router = Router()
+
+@router.message(CommandStart())
+async def cmd_start(message: Message):   
+    await message.reply("Привет, я бот") 
+
+@router.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.reply("Привет, это помощь!")
+
+@router.message(F.text == "тест")
+async def cmd_test(message: Message):
+    await message.reply("Привет, это тестовое сообщение!")
