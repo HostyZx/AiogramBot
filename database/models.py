c91e45d840eaad3020cfdf5e5292124d6cd74443
hostyz <andreydiz1234@gmail.com>
2024-10-27 15:00:05 +0000

    1.2

--- database/models.py
+++ database/models.py
@@ -1,30 +1,31 @@
+
 from sqlalchemy import BigInteger, String, Column, ForeignKey
-from sqlalchemy.orm import Declarative_base, Mapped, mapped_collumns
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AssyncAttrs, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_async_engine(url="sqlite+aiosqlite:///database.sqlite3")
+create_engine = create_async_engine(url='sqlite+aiosqlite:///database.sqlite3')
 
-async_sessionmaker = async_sessionmaker(engine)
+async_sessionmaker = async_sessionmaker(create_engine, echo=True)
 
-class Base(AssyncAttrs, Declarative_base):
+class Base(AssyncAttrs, DeclarativeBase):
     pass
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_collumns(id, primary_key=True)
-    tg_id = mapped_collumns(BigInteger)
+    id: Mapped[int] = mapped_column(id, primary_key=True)
+    tg_id = mapped_column(BigInteger)
 
-class Profile(User):
+class Profile(Base):
     __tablename__ = 'profiles'
 
-    id: Mapped[int] = mapped_collumns(ForeignKey(User.id), primary_key=True)
-    name: Mapped[str] = mapped_collumns(String(25), nullable=True)
-    age: Mapped[int] = mapped_collumns(int, nullable=True)
-    city: Mapped[str] = mapped_collumns(String(120), nullable=True)
-    phone: Mapped[str] = mapped_collumns(String(40), nullable=True)
+    id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
+    name: Mapped[str] = mapped_column(String(25))
+    age: Mapped[int] = mapped_column(int)
+    city: Mapped[str] = mapped_column(String(120))
+    phone: Mapped[str] = mapped_column(String(40))
 
 async def async_main():
-    async with engine.begin() as conn:
+    async with create_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
