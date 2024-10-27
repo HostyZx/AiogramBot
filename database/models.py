94925f3d2d4cf9ebb7f406b7de46bba80064ae83
hostyz <andreydiz1234@gmail.com>
2024-10-27 15:11:57 +0000

    1.3

--- database/models.py
+++ database/models.py
@@ -1,30 +1,31 @@
 
 from sqlalchemy import BigInteger, String, Column, ForeignKey
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy.ext.asyncio import AssyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 create_engine = create_async_engine(url='sqlite+aiosqlite:///database.sqlite3')
 
 async_sessionmaker = async_sessionmaker(create_engine, echo=True)
 
-class Base(AssyncAttrs, DeclarativeBase):
+class Base(AsyncAttrs, DeclarativeBase):
     pass
 
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(id, primary_key=True)
-    tg_id = mapped_column(BigInteger)
+    user_id: Mapped[int] = Column(BigInteger, primary_key=True)
+    tg_id = Column(BigInteger)
 
-class Profile(Base):
-    __tablename__ = 'profiles'
+# class Profile(Base):
+#     __tablename__ = 'profiles'
 
-    id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
-    name: Mapped[str] = mapped_column(String(25))
-    age: Mapped[int] = mapped_column(int)
-    city: Mapped[str] = mapped_column(String(120))
-    phone: Mapped[str] = mapped_column(String(40))
+#     id: Mapped[int] = mapped_column(int, primary_key=True)
+#     user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+#     name: Mapped[str] = mapped_column(String(25))
+#     age: Mapped[int] = mapped_column(int)
+#     city: Mapped[str] = mapped_column(String(120))
+#     phone: Mapped[str] = mapped_column(String(40))
 
 async def async_main():
     async with create_engine.begin() as conn:
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import logging
 from aiogram import Bot, Dispatcher
 from app.handlers import router
 from config import TELEGRAM_TOKEN
-#from database.models import async_main
+from database.models import async_main
 
 
 async def main():
-   #await async_main()  # create tables in the database
+    await async_main()  # create tables in the database
     bot = Bot(token=TELEGRAM_TOKEN)
     dp = Dispatcher()
     dp.include_router(router)
